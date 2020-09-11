a942300cd78b593a762908dc825a3c4e74e131d8
Faye Jakovcevic <fayejakovcevic@Fayes-MacBook-Pro.local>
2020-09-11 09:47:54 +0800

    set up sign in and posting to dynamo now

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,42 @@ import React from 'react';
 import './App.css';
 import AppBar from './components/AppBar'
 import {Button} from '@material-ui/core'
+import { createCards } from './graphql/mutations';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { AmplifySignOut, AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <AppBar user="Faye"/>
-      <Button variant="contained" color="primary">Test</Button>
-    </div>
-  );
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+        setAuthState(nextAuthState);
+        setUser(authData)
+    });
+}, []);
+
+  const add = async () => {
+    const add = { id: "id", category: "SNS", question: "Question", answer:"answer" };
+    try{
+      await API.graphql(graphqlOperation(createCards, {input: add}))
+    }
+    catch(e){
+      console.log('Error',e)
+    }
+  }
+
+return authState === AuthState.SignedIn && user ? (
+  <div className="App">
+          <AppBar user={user.username} signOut={<AmplifySignOut/>}/>
+          <Button onClick={()=>add()}variant="contained" color="primary">Test</Button>
+  </div>
+) : (
+  <AmplifyAuthenticator />
+);
 }
 
 export default App;
